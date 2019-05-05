fdd765409e3a4b5ee9db3b83c88f79106eb84748
Simon Künzi <sim.kuenzi@gmail.com>
2019-05-05 12:50:55 +0200

    Use MapForm for demo showcase

--- src/main/java/com/github/simkuenzi/restforms/demo/DemoResource.java
+++ src/main/java/com/github/simkuenzi/restforms/demo/DemoResource.java
@@ -1,5 +1,6 @@
 package com.github.simkuenzi.restforms.demo;
 
+import com.github.simkuenzi.restforms.*;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,27 +29,22 @@ public class DemoResource {
 
     @GET
     public String get() {
-        Map<String, Object> vars = new HashMap<>();
-        vars.put("form", new DemoForm().noValidation());
-        return render(vars);
+        return render(form(new MultivaluedHashMap<>()).nonValidating());
     }
 
     @POST
     public String post(MultivaluedMap<String, String> rawForm) {
-        DemoForm form = new DemoForm(rawForm);
+        MapForm form = form(rawForm);
         if (form.valid()) {
             System.out.println("Form is valid");
         } else {
             System.out.println("Form is invalid");
         }
 
-        Map<String, Object> vars = new HashMap<>();
-        vars.put("form", form);
-
-        return render(vars);
+        return render(form);
     }
 
-    private String render(Map<String, Object> vars) {
+    private String render(MapForm form) {
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
         templateResolver.setTemplateMode(TemplateMode.HTML);
         templateResolver.setPrefix("/WEB-INF/templates/");
@@ -57,7 +54,19 @@ public class DemoResource {
         TemplateEngine templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
 
+        Map<String, Object> vars = new HashMap<>();
+        vars.put("form", form);
+
         WebContext context = new WebContext(servletRequest, servletResponse, servletContext, servletRequest.getLocale(), vars);
         return templateEngine.process("form", context);
+    }
+
+    private MapForm form(MultivaluedMap<String, String> rawForm) {
+        return new MapForm("Invalid Input",
+                new TextField(new FormValue("text", rawForm)),
+                new MandatoryField(
+                        new TextField(new FormValue("mandatoryText", rawForm)), "This field is mandatory"),
+                new DecimalField(new FormValue("decimal", rawForm),"Must be a valid decimal number")
+        );
     }
 }
